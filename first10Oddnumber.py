f764fc479a437fc85563b76ed51578aa35083809
Amrit khatri <amritkhatri95@gmail.com>
2022-08-01 14:48:56 -0400

    this is first commit

---
+++ first10Oddnumber.py
@@ -0,0 +1,18 @@
+"""print 10 odd number.
+1.declare a variable with name i and assign it with value 0
+2.declare another variable with name n and assign it with 1
+3.run while loop till i<11
+4.use if condition if n modulus value with 2 is 1
+5. print n
+6.increment the value of i by 1 each time 
+7. increment hte value of n by 1 each time 
+"""
+i=0#(no of time its printed)
+n=1#(incremental number)
+while i<10:
+    if n%2==1:
+
+
+       print(n)
+       i=i+1#(increment the value of i each time)
+    n=n+1#(increment the value of n each time )
